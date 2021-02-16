b33db70b9b26997950753f10ebb0ba39159112c6
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2021-02-16 16:35:54 +0100

    Add files via upload

---
+++ Pioche.java
@@ -0,0 +1,26 @@
+package appli;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+public class Pioche {
+    public static final int NBCARTESPIOCHE = 60;
+    private List<Integer> element = new ArrayList<>();
+
+    public List<Integer> getElement() {
+        return element;
+    }
+
+    public Pioche() {
+        for(int n=1; n<=NBCARTESPIOCHE; n++) {
+            element.add(n);
+        }
+    }
+    public Integer tirage(){
+        Random r = new Random();
+        int a = r.nextInt(element.size());
+        Integer iter = element.get(a);
+        element.remove(a);
+        return iter;
+    }
+}
