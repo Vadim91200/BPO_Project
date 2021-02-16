39f1f478c5f83f032edc07c136abdc18852bce3f
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2021-02-16 19:46:34 +0100

    Update FonctionsJoueur.java

--- FonctionsJoueur.java
+++ FonctionsJoueur.java
@@ -1,6 +1,7 @@
 package appli;
 
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 public class FonctionsJoueur {
     public static boolean fini(Joueur j) {
         return perdu(j) || gagne(j);
@@ -27,6 +28,7 @@ public class FonctionsJoueur {
             }
         } else {
             int c=0, g=0;
+            boolean isValid = true;
             String x = "";
             do {
                 System.out.print("> ");
@@ -35,21 +37,24 @@ public class FonctionsJoueur {
                 Scanner k = new Scanner(s);
                 while (k.hasNext()) {
                     x = k.next();
-                    if (EstValide(x, j1)) {
+                    try { String u = k.next(); }
+                    catch(NoSuchElementException nfe){ isValid = false; }
+                    if (EstValide(x, j1,j2) == true && isValid == true) {
                         Ajouter(x, j1, j2);
                         c++;
                     }else{
                         g++;
                     }
                 }
-                Remplissage(j1, s, c);
-                if (g==0){
+                if (g==0 && isValid == false){
+                    Remplissage(j1, s, c);
                     break;
                 }
                 System.out.print("#");
-                }while (!EstValide(x,j1));
+                g=0;
+                }while (EstValide(x, j1,j2) == false && isValid == true);
+            j1.ChangementTour();
         }
-        j1.ChangementTour();
     }
     private static void Remplissage(Joueur j1, String s, int c){
         if (j1.getPcarte().getElement().size()<6) {
@@ -73,17 +78,47 @@ public class FonctionsJoueur {
             System.out.println(c + " cartes posées, 2 cartes piochées ");
         }
     }
-    public static boolean EstValide(String s, Joueur j) {
+    public static boolean EstValide(String s, Joueur j, Joueur j2) {
+        if (s.length()<3){
+            return false;
+        }
+        boolean isValid = true;
+        String NB = s.substring(0, 2);
+        try{ Integer.parseInt(NB); }
+        catch(NumberFormatException nfe){ isValid = false; }
+        if (isValid){
+            if(!j.getMain().contains(Integer.parseInt(NB))){
+                return false;
+            }
+            String Crochet = s.substring(2,3);
+            if(Crochet.equals("v") || Crochet.equals("^")){
+                if (s.contains("'")) {
+                    String Apostrophe = s.substring(3, 4);
+                    if (Apostrophe.equals("'")){
+                        return (EstPosable(s,j,j2, NB));
+                    }else return false;
+                }else return true;
+            }else return false;
+
+        }return false;
+    }
+    private static boolean EstPosable(String s, Joueur j, Joueur j2, String NB) {
         if (s.contains("'")){
+            if (s.contains("^")) {
+                return (Integer.parseInt(NB)<j2.getAscendant());
+            }else{
 
+                return (Integer.parseInt(NB)>j2.getDescendant());
+            }
         }else if (s.contains("^")){
-
+            return (Integer.parseInt(NB)>j.getAscendant() || (j.getAscendant() - Integer.parseInt(NB)) == 10);
         }else if (s.contains("v")){
-
+            return (Integer.parseInt(NB)<j.getDescendant() || (Integer.parseInt(NB)-j.getDescendant()) == 10);
         }
         return false;
     }
     public static void Ajouter(String s, Joueur j1, Joueur j2) {
+        assert(EstValide(s,j1,j2));
         String k = s.substring(0, 2);
         if (s.contains("'")){
         	if (s.contains("^")){
