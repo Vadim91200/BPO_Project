f88fe22abf30910d5947a1f8742719b0a96396b1
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2021-02-16 16:34:59 +0100

    Update FonctionsJoueur.java

--- FonctionsJoueur.java
+++ FonctionsJoueur.java
@@ -1,77 +1,103 @@
 package appli;
 
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Collections;
 public class FonctionsJoueur {
-    public static void Initialiser(Joueur Entré) {
-        Entré.Pioche = 52;
-        Entré.ascendant.push(01);
-        Entré.descendant.push(60);
-        for(int n=0; n<6; n++){
-            Random r = new Random();
-            Entré.main.add(r.nextInt(52));
-        }
-        Collections.sort(Entré.main);
-    }
-    public static boolean fini(boolean b, Joueur j1, Joueur j2 ) {
-        if(b){
-            return perdu(j1) || gagne(j1);
-        }
-        else{
-            return perdu(j2) || gagne(j2);
-        }
+    public static boolean fini(Joueur j) {
+        return perdu(j) || gagne(j);
     }
 
     public static boolean perdu(Joueur j) {
-        return j.main.size()<2;
+        return j.getMain().size()<2;
     }
 
     public static boolean gagne(Joueur j) {
         if (perdu(j))
             return false;
-        if (j.main.size()>0)
+        if (j.getMain().size()>0)
             return false;
         return true;
     }
 
-    public static boolean jouer(boolean b, Joueur j1, Joueur j2) {
-        if (fini(b, j1, j2)) {
-            if (b = true) {
+    public static void jouer(Joueur j1, Joueur j2) {
+        if (fini(j1)) {
+            if (j1.getTour()%2==0) {
                  System.out.println("partie finie, SUD a gagne");
-                 return b;
             } else {
                 System.out.println("partie finie, NORD a gagne");
-                return b;
             }
         } else {
-            System.out.print("> ");
-            Scanner sc = new Scanner(System.in);
-            String s = sc.nextLine();
-            System.out.println(s);
-            Scanner k = new Scanner(s);
-            while(k.hasNext()) {
-                String x = k.next();
-                if(EstValide(x)){
-
+            int c=0, g=0;
+            String x = "";
+            do {
+                System.out.print("> ");
+                Scanner sc = new Scanner(System.in);
+                String s = sc.nextLine();
+                Scanner k = new Scanner(s);
+                while (k.hasNext()) {
+                    x = k.next();
+                    if (EstValide(x, j1)) {
+                        Ajouter(x, j1, j2);
+                        c++;
+                    }else{
+                        g++;
+                    }
                 }
-
-            }
-            j1.main.remove(0);
+                Remplissage(j1, s, c);
+                if (g==0){
+                    break;
+                }
+                System.out.print("#");
+                }while (!EstValide(x,j1));
         }
-        return b = !b;
+        j1.ChangementTour();
     }
-    public static boolean EstValide(String s) {
-        boolean b =true;
-        return b;
+    private static void Remplissage(Joueur j1, String s, int c){
+        if (j1.getPcarte().getElement().size()<6) {
+            if (j1.getPcarte().getElement().size() == 0) {
+                System.out.println(c + " cartes posées, 0 cartes piochées ");
+            } else {
+                System.out.println(c + " cartes posées, " + j1.getPcarte().getElement().size() + " cartes piochées ");
+                j1.remplirMain(j1.getPcarte().getElement().size());
+            }
+        }
+        else if (s.contains("'")) {
+            int i = 0;
+            while (j1.getMain().size() < 6) {
+                j1.remplirMain(1);
+                i++;
+            }
+            System.out.println(c + " cartes posées, " + i + " cartes piochées ");
+
+        } else if (s.contains("^")) {
+            j1.remplirMain(2);
+            System.out.println(c + " cartes posées, 2 cartes piochées ");
+        }
     }
-    public static void Ajouter(String s) {
+    public static boolean EstValide(String s, Joueur j) {
         if (s.contains("'")){
 
         }else if (s.contains("^")){
 
         }else if (s.contains("v")){
 
+        }
+        return false;
+    }
+    public static void Ajouter(String s, Joueur j1, Joueur j2) {
+        String k = s.substring(0, 2);
+        if (s.contains("'")){
+        	if (s.contains("^")){
+                j2.PileAscendante().push(Integer.parseInt(k));
+            }else{
+                j2.PileDescendante().push(Integer.parseInt(k));
+            }
+            j1.getMain().remove((Integer) Integer.parseInt(k));
+        }else if (s.contains("^")){
+            j1.PileAscendante().push(Integer.parseInt(k));
+            j1.getMain().remove((Integer) Integer.parseInt(k));
+        }else if (s.contains("v")){
+            j1.PileDescendante().push(Integer.parseInt(k));
+            j1.getMain().remove((Integer) Integer.parseInt(k));
         }
     }
 }
