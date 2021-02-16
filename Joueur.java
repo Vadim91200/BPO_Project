f0eec11614606b03dd2fa570ee5845bb5a890beb
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2021-02-16 16:35:20 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,11 +1,51 @@
 package appli;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 public class Joueur {
-    Stack<Integer> ascendant = new Stack<Integer>();
-    Stack<Integer> descendant = new Stack<Integer>();
-    ArrayList<Integer> main = new ArrayList<>();
-    int Pioche;
+    public static final int nbCarteMain = 6;
+    private Stack<Integer> ascendant = new Stack<Integer>();
+    private Stack<Integer> descendant = new Stack<Integer>();
+    private ArrayList<Integer> main = new ArrayList<>();
+    private Pioche pcarte = new Pioche();
+    private static int Tour;
+
+    public Joueur(){
+        ascendant.push(pcarte.getElement().get(0));
+        pcarte.getElement().remove(0);
+        descendant.push(pcarte.getElement().get(58));
+        pcarte.getElement().remove(58);
+        remplirMain(nbCarteMain);
+        Tour = 0;
+    }
+    public Pioche getPcarte() {
+        return pcarte;
+    }
+    public Integer getAscendant() {
+        return ascendant.peek();
+    }
+    public Integer getDescendant() {
+        return descendant.peek();
+    }
+    public Stack<Integer> PileAscendante(){
+        return ascendant;
+    }
+    public Stack<Integer> PileDescendante() {
+        return descendant;
+    }
+
+    public ArrayList<Integer> getMain() {
+        return main;
+    }
+    public void remplirMain(int l){
+        for(int i = 0; i<l; i++ ){
+            main.add(pcarte.tirage());
+        }
+        Collections.sort(main);
+    }
+    public int getTour(){ return Tour;}
+    public void ChangementTour(){ Tour++;}
 }
+
